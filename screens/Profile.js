e4a6c81756679dfc9b3539a07334943787e23e36
moses1206 <33392504+moses1206@users.noreply.github.com>
2022-06-08 22:44:57 +0900

    2

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,8 +1,19 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, Linking, Platform } from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
+import { Title, Card } from 'react-native-paper'
+import { MaterialIcons, Entypo, AntDesign } from '@expo/vector-icons'
+import { Button } from 'react-native-paper'
 
 export default function Profile() {
+  const openDial = () => {
+    if (Platform.OS === 'android') {
+      Linking.openURL('tel:12345')
+    } else {
+      Linking.openURL('telprompt:12345')
+    }
+  }
+
   return (
     <View style={styles.root}>
       <LinearGradient
@@ -17,12 +28,79 @@ export default function Profile() {
           }}
         />
       </View>
+      <View style={{ alignItems: 'center', margin: 15 }}>
+        <Title>Jang Bu Ho</Title>
+        <Text style={{ fontSize: 15 }}>Full Stack Developer</Text>
+      </View>
+      <Card
+        style={styles.mycard}
+        onPress={() => {
+          Linking.openURL('mailto:abc@abc.com')
+        }}
+      >
+        <View style={styles.cardContent}>
+          <MaterialIcons name='email' size={32} color='#006aff' />
+          <Text style={styles.mytext}>abc@naver.com</Text>
+        </View>
+      </Card>
+      <Card style={styles.mycard} onPress={() => openDial()}>
+        <View style={styles.cardContent}>
+          <Entypo name='phone' size={32} color='#006aff' />
+          <Text style={styles.mytext}>125356</Text>
+        </View>
+      </Card>
+      <Card style={styles.mycard}>
+        <View style={styles.cardContent}>
+          <MaterialIcons name='attach-money' size={32} color='#006aff' />
+          <Text style={styles.mytext}>8 LPA</Text>
+        </View>
+      </Card>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          padding: 10,
+        }}
+      >
+        <Button
+          icon='account-edit'
+          mode='contained'
+          theme={theme}
+          onPress={() => console.log('Pressed')}
+        >
+          Edit
+        </Button>
+        <Button
+          icon='delete'
+          mode='contained'
+          theme={theme}
+          onPress={() => console.log('Pressed')}
+        >
+          Fire employee
+        </Button>
+      </View>
     </View>
   )
+}
+
+const theme = {
+  colors: { primary: '#006aff' },
 }
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  mycard: {
+    margin: 3,
+  },
+  cardContent: {
+    flexDirection: 'row',
+    padding: 8,
+  },
+  mytext: {
+    fontSize: 15,
+    marginTop: 3,
+    marginLeft: 5,
   },
 })
